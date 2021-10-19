5d44adf148ec5bc86311bdff103834f06bef9454
HUANG HAO <934303306@qq.com>
2021-10-19 11:56:22 +0800

    重构

--- v2panel/settings.py
+++ v2panel/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'v2panel.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / config('DATA_BASE'),
     }
 }
 
